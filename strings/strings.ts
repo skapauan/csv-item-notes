059ffb5cb7b1b525dcf752d2ca7ca686d34658fd
skapauan <skapauan@users.noreply.github.com>
2021-05-29 23:04:38 -0500

    Localization

--- strings/strings.ts
+++ strings/strings.ts
@@ -1,10 +1,12 @@
-import LocalizedStrings from 'react-native-localization'
+import LocalizedStrings from 'localized-strings'
+import * as Localization from 'expo-localization'
 
 export const Strings = new LocalizedStrings({
-    'en-US': {
+    en: {
         ErrorInvalidNumberRows: 'Invalid CSV: At least one row of data is required, not including the header row',
         ErrorInvalidNumberColumns: 'Invalid CSV: At least one column of data is required',
         ErrorInvalidFieldVariance: 'Invalid CSV: All rows must have same number of fields',
         ErrorUnknown: 'Unknown Error',
     }
 })
+Strings.setLanguage(Localization.locale)
